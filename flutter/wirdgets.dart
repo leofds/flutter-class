a2dc510165c2796fb582725da37015e81e8eb9ba
Leonardo Fernandes <leonardo_.fernandes@hotmail.com>
2023-06-27 00:14:42 -0300

    Create wirdgets.dart

---
+++ flutter/wirdgets.dart
@@ -0,0 +1,13 @@
+# Widgets
+
+Relação dos principais Widgets e suas propriedades mais comuns
+
+## MaterialApp
+
+```dart
+MaterialApp(
+  debugShowCheckedModeBanner: false,    // Retira a tarja de debug
+  title: "Meu App",
+  home: <Widget?>,
+)
+```
